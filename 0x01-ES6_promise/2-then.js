460af5dcfbca779e5af1b5efa55971d4b134e025
Geff115 <gabrielnoah129@gmail.com>
2024-10-04 00:57:25 +0100

    Using .then, .catch, and .finally handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, response: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
+
+export default handleResponseFromAPI;
