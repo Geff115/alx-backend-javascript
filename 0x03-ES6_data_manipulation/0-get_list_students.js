23165a51e5673072862f97ab0a1fb5aefb78f76c
Geff115 <gabrielnoah129@gmail.com>
2024-10-07 08:09:45 +0100

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function getListStudents() {
+  const arrayObjects = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return arrayObjects;
+}
+
+export default getListStudents;
