734afbaae3bd64639771919a3c7008780336a057
Geff115 <gabrielnoah129@gmail.com>
2024-10-07 18:49:33 +0100

    Added some type checking

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,26 @@
 function cleanSet(set, startString) {
+  // Ensuring set is a Set object and startString is a string
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  // Return an empty string if the startString is empty
   if (startString === '') {
     return '';
   }
   const listOfString = [];
+
+  // Iterate over the set and find values that start with startString
   for (const element of set) {
     if (element.startsWith(startString)) {
+      // Append the rest of the string after startString
       const restOfString = element.substring(startString.length);
       listOfString.push(restOfString);
     }
   }
-  const newString = listOfString.join('-');
 
-  return newString;
+  // Join the values with '-' and return the result
+  return listOfString.join('-');
 }
 
 export default cleanSet;
